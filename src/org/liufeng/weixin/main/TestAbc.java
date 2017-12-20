d1aa42e61dbcbb4702389d96c3f4b9d6ee266212
Administrator <Administrator@MS-20170608JDNZ>
2017-12-20 17:42:04 +0800

    TESETAC

---
+++ src/org/liufeng/weixin/main/TestAbc.java
@@ -0,0 +1,13 @@
+package org.liufeng.weixin.main;
+
+public class TestAbc {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
