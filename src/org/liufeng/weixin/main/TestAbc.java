b71d8d19360be2dd5d62960d2e67f6a3e564335e
Administrator <Administrator@MS-20170608JDNZ>
2017-12-21 09:07:04 +0800

    third remark

--- src/org/liufeng/weixin/main/TestAbc.java
+++ src/org/liufeng/weixin/main/TestAbc.java
@@ -8,6 +8,8 @@ public class TestAbc {
 	public static void main(String[] args) {
 		System.out.println("WFWEFWEFWEFWEFWEFWEFWEFWE");
 		System.out.println("------------A------------");
+		System.out.println("------------B------------");
+		System.out.println("------------C------------");
 	}
 
 }
